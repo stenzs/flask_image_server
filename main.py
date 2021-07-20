9f5957c9f36bc7727636145550eec945fb53f5fc
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-07-20 16:14:01 +0500

    v0.1

--- main.py
+++ main.py
@@ -4,12 +4,16 @@ import psycopg2
 
 app = Flask(__name__)
 
+
+# База данных
 database = "kvik"
 user = "kvik"
 password = "2262"
 host = "192.168.8.92"
 port = "5432"
 
+
+# Конфигурация
 AVATAR_UPLOAD_FOLDER = 'static/avatars'
 app.config['AVATAR_UPLOAD_FOLDER'] = AVATAR_UPLOAD_FOLDER
 POST_UPLOAD_FOLDER = 'static/posts'
@@ -18,176 +22,173 @@ HOST = '127.0.0.1'
 PORT = 80
 
 
+# На случай проверки формата
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower()
 
 
+# Установка нового аватара
 @app.route('/avatar/<user_id>', methods=['POST'])
 def upload_avatar(user_id):
-    if 'files[]' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
-        resp.status_code = 400
-        return resp
-    files = request.files.getlist('files[]')
-    errors = {}
-    success = False
-    for file in files:
-        if not os.path.exists(AVATAR_UPLOAD_FOLDER + '/' + user_id):
-            os.makedirs(AVATAR_UPLOAD_FOLDER + '/' + user_id)
-        if file and allowed_file(file.filename):
-            old_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, "avatar.webp")
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            resp = jsonify({'message': 'No file part in the request'})
+            resp.status_code = 400
+            return resp
+        files = request.files.getlist('files[]')
+        errors = {}
+        success = False
+        for file in files:
+            if not os.path.exists(AVATAR_UPLOAD_FOLDER + '/' + user_id):
+                os.makedirs(AVATAR_UPLOAD_FOLDER + '/' + user_id)
+            if file and allowed_file(file.filename):
+                old_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, "avatar.webp")
 
-            path, dirs, files = next(os.walk(AVATAR_UPLOAD_FOLDER + '/' + user_id))
-            file_count = str(len(files))
-            if len(files) != 0:
-                new_name = 'avatar(' + file_count + ').webp'
-                new_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, new_name)
-                os.rename(old_file, new_file)
-            filename = 'avatar.webp'
-            file.save(os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, filename))
-            road = 'http://' + HOST + '/static/avatars/' + user_id + '/avatar.webp'
+                path, dirs, files = next(os.walk(AVATAR_UPLOAD_FOLDER + '/' + user_id))
+                file_count = str(len(files))
+                if len(files) != 0:
+                    new_name = 'avatar(' + file_count + ').webp'
+                    new_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, new_name)
+                    os.rename(old_file, new_file)
+                filename = 'avatar.webp'
+                file.save(os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, filename))
+                road = 'http://' + HOST + '/static/avatars/' + user_id + '/avatar.webp'
 
-            success = True
+                success = True
+            else:
+                errors[file.filename] = 'File type is not allowed'
+
+        if success and errors:
+            errors['message'] = 'File(s) successfully uploaded'
+            resp = jsonify(errors)
+            resp.status_code = 500
+
+            con = psycopg2.connect(
+                database=database,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            cur = con.cursor()
+            row = '"userPhoto"'
+            cur.execute(
+                "UPDATE public.users SET " + row + " = '" + road + "' WHERE id = " + user_id
+            )
+            con.commit()
+            con.close()
+
+            return resp
+
+        if success:
+            resp = jsonify({'message': 'Files successfully uploaded'})
+            resp.status_code = 201
+
+            con = psycopg2.connect(
+                database=database,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            cur = con.cursor()
+            row = '"userPhoto"'
+            cur.execute(
+                "UPDATE public.users SET " + row + " = '" + road + "' WHERE id = " + user_id
+            )
+            con.commit()
+            con.close()
+
+            return resp
         else:
-            errors[file.filename] = 'File type is not allowed'
-
-    if success and errors:
-        errors['message'] = 'File(s) successfully uploaded'
-        resp = jsonify(errors)
-        resp.status_code = 500
-
-        con = psycopg2.connect(
-            database=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        cur = con.cursor()
-        row = '"userPhoto"'
-        cur.execute(
-            "UPDATE public.users SET " + row + " = '" + road + "' WHERE id = " + user_id
-        )
-        con.commit()
-        con.close()
-
-        return resp
-
-    if success:
-        resp = jsonify({'message': 'Files successfully uploaded'})
-        resp.status_code = 201
-
-        con = psycopg2.connect(
-            database=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        cur = con.cursor()
-        row = '"userPhoto"'
-        cur.execute(
-            "UPDATE public.users SET " + row + " = '" + road + "' WHERE id = " + user_id
-        )
-        con.commit()
-        con.close()
-
-        return resp
-    else:
-        resp = jsonify(errors)
-        resp.status_code = 500
-        return resp
-
-
-
-
-
-
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
 
     # Добавить добавление фото (одного или нескольких) но в пределах ограничения
     # Добавить изменение главного фото
     # Добавить удаление фото
 
-
-
+# Манипуляции с фото постов
 @app.route('/post/<post_id>', methods=['POST'])
 def upload_post_photo(post_id):
-    if 'files[]' not in request.files:
-        resp = jsonify({'message': 'No file part in the request'})
-        resp.status_code = 400
-        return resp
-    files = request.files.getlist('files[]')
-    errors = {}
-    success = False
-    if not os.path.exists(POST_UPLOAD_FOLDER + '/' + post_id):
-        os.makedirs(POST_UPLOAD_FOLDER + '/' + post_id)
+    if request.method == 'POST':
+        if 'files[]' not in request.files:
+            resp = jsonify({'message': 'No file part in the request'})
+            resp.status_code = 400
+            return resp
+        files = request.files.getlist('files[]')
+        errors = {}
+        success = False
+        if not os.path.exists(POST_UPLOAD_FOLDER + '/' + post_id):
+            os.makedirs(POST_UPLOAD_FOLDER + '/' + post_id)
 
-    data = {"photos": []}
+        data = {"photos": []}
 
-    for file in files:
-        if file and allowed_file(file.filename):
-            path, dirs, files = next(os.walk(POST_UPLOAD_FOLDER + '/' + post_id))
-            file_count = str(len(files) + 1)
-            new_name = 'post(' + file_count + ').webp'
-            file.save(os.path.join(POST_UPLOAD_FOLDER + '/' + post_id, new_name))
-            road = 'http://' + HOST + '/static/posts/' + post_id + '/' + new_name
-            data["photos"].append(road)
-            success = True
+        for file in files:
+            if file and allowed_file(file.filename):
+                path, dirs, files = next(os.walk(POST_UPLOAD_FOLDER + '/' + post_id))
+                file_count = str(len(files) + 1)
+                new_name = 'post(' + file_count + ').webp'
+                file.save(os.path.join(POST_UPLOAD_FOLDER + '/' + post_id, new_name))
+                road = 'http://' + HOST + '/static/posts/' + post_id + '/' + new_name
+                data["photos"].append(road)
+                success = True
+
+            else:
+                errors[file.filename] = 'File type is not allowed'
+
+        if success and errors:
+            errors['message'] = 'File(s) successfully uploaded'
+            resp = jsonify(errors)
+            resp.status_code = 500
+            valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
+
+            con = psycopg2.connect(
+                database=database,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            cur = con.cursor()
+            row = '"photo"'
+            cur.execute(
+                "UPDATE public.posts SET " + row + " = '" + valid_data + "' WHERE id = " + post_id
+            )
+            con.commit()
+            con.close()
+
+            return resp
+
+        if success:
+
+            resp = jsonify({'message': 'Files successfully uploaded'})
+            resp.status_code = 201
+            valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
+
+            con = psycopg2.connect(
+                database=database,
+                user=user,
+                password=password,
+                host=host,
+                port=port
+            )
+            cur = con.cursor()
+            row = '"photo"'
+            cur.execute(
+                "UPDATE public.posts SET " + row + " = '" + valid_data + "' WHERE id = " + post_id
+            )
+            con.commit()
+            con.close()
+
+            return resp
 
         else:
-            errors[file.filename] = 'File type is not allowed'
 
-    if success and errors:
-        errors['message'] = 'File(s) successfully uploaded'
-        resp = jsonify(errors)
-        resp.status_code = 500
-        valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
-
-        con = psycopg2.connect(
-            database=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        cur = con.cursor()
-        row = '"photo"'
-        cur.execute(
-            "UPDATE public.posts SET " + row + " = '" + valid_data + "' WHERE id = " + post_id
-        )
-        con.commit()
-        con.close()
-
-        return resp
-
-    if success:
-
-        resp = jsonify({'message': 'Files successfully uploaded'})
-        resp.status_code = 201
-        valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
-
-        con = psycopg2.connect(
-            database=database,
-            user=user,
-            password=password,
-            host=host,
-            port=port
-        )
-        cur = con.cursor()
-        row = '"photo"'
-        cur.execute(
-            "UPDATE public.posts SET " + row + " = '" + valid_data + "' WHERE id = " + post_id
-        )
-        con.commit()
-        con.close()
-
-        return resp
-
-    else:
-
-        resp = jsonify(errors)
-        resp.status_code = 500
-        return resp
+            resp = jsonify(errors)
+            resp.status_code = 500
+            return resp
 
 
 if __name__ == '__main__':
