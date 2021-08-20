48ef6f652b732d9b2b86a8bd0e0274f5e589c9ca
Sergey Zinyaev <zinyaevsi@gmail.com>
2021-08-20 15:34:55 +0500

    /

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 from flask import Flask, request, jsonify
-import os
-import psycopg2
 from flask_cors import CORS
+import os
+import hashlib
+from datetime import datetime
+import uuid
+import psycopg2
 import config
 
-app = Flask(__name__)
+app = Flask(__name__, static_url_path="/images")
 CORS(app)
 
 database = config.database
@@ -13,74 +16,54 @@ password = config.password
 host = config.host
 port = config.port
 
+ADDRESS = config.ADDRESS
 AVATAR_UPLOAD_FOLDER = config.AVATAR_UPLOAD_FOLDER
+AVATAR_UPLOAD_ALIAS = config.AVATAR_UPLOAD_ALIAS
 POST_UPLOAD_FOLDER = config.POST_UPLOAD_FOLDER
-app.config['AVATAR_UPLOAD_FOLDER'] = AVATAR_UPLOAD_FOLDER
-app.config['POST_UPLOAD_FOLDER'] = POST_UPLOAD_FOLDER
-HOST = config.HOST
-PORT = config.PORT
+POST_UPLOAD_ALIAS = config.POST_UPLOAD_ALIAS
 
 
 def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower()
 
 
+@app.route('/', methods=['GET'])
+def hello():
+    if request.method == 'GET':
+        return jsonify({'message': 'test'}), 200
+
+
 @app.route('/avatar/<user_id>', methods=['POST'])
 def upload_avatar(user_id):
     if request.method == 'POST':
         if 'files[]' not in request.files:
-            resp = jsonify({'message': 'No file part in the request'})
-            resp.status_code = 400
-            return resp
+            return jsonify({'message': 'No file part in the request'}), 422
         files = request.files.getlist('files[]')
+
         errors = {}
         success = False
         for file in files:
-            if not os.path.exists(AVATAR_UPLOAD_FOLDER + '/' + user_id):
-                os.makedirs(AVATAR_UPLOAD_FOLDER + '/' + user_id)
-            if file and allowed_file(file.filename):
-                old_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, "avatar.webp")
-
-                path, dirs, files = next(os.walk(AVATAR_UPLOAD_FOLDER + '/' + user_id))
-                file_count = str(len(files))
-                if len(files) != 0:
-                    new_name = 'avatar(' + file_count + ').webp'
-                    new_file = os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, new_name)
-                    os.rename(old_file, new_file)
-                filename = 'avatar.webp'
-                file.save(os.path.join(AVATAR_UPLOAD_FOLDER + '/' + user_id, filename))
-                road = 'http://' + HOST + ":" + str(PORT) + '/' + AVATAR_UPLOAD_FOLDER + '/' + user_id + '/avatar.webp'
-
+            if file:
+                data = file.read()
+                file_hash = hashlib.md5(data).hexdigest()
+                filename = file_hash[10:] + (uuid.uuid4().hex)[0:7] + datetime.now().strftime('%Y%m%d%H%M%S%f') + '.webp'
+                hash_road = str(file_hash[0:2]) + '/' + str(file_hash[2:4]) + '/' + str(file_hash[4:6]) + '/' + str(file_hash[6:8])
+                if not os.path.exists(AVATAR_UPLOAD_FOLDER + '/' + hash_road):
+                    os.makedirs(AVATAR_UPLOAD_FOLDER + '/' + hash_road)
+                f = open(AVATAR_UPLOAD_FOLDER + '/' + hash_road + '/' + filename, "wb")
+                f.write(data)
+                f.close()
+                road = AVATAR_UPLOAD_ALIAS + '/' + hash_road + '/' + filename
                 success = True
             else:
                 errors[file.filename] = 'File type is not allowed'
 
         if success and errors:
-            errors['message'] = 'File(s) successfully uploaded'
+            errors['message'] = 'something wrong'
             resp = jsonify(errors)
             resp.status_code = 500
-
-            con = psycopg2.connect(
-                database=database,
-                user=user,
-                password=password,
-                host=host,
-                port=port
-            )
-            cur = con.cursor()
-            row = '"userPhoto"'
-            cur.execute(
-                "UPDATE public.users SET " + row + " = '" + road + "' WHERE id = " + user_id
-            )
-            con.commit()
-            con.close()
-
             return resp
-
         if success:
-            resp = jsonify({'message': 'Files successfully uploaded'})
-            resp.status_code = 201
-
             con = psycopg2.connect(
                 database=database,
                 user=user,
@@ -95,72 +78,48 @@ def upload_avatar(user_id):
             )
             con.commit()
             con.close()
-
-            return resp
+            return jsonify({'message': 'success', 'image': road}), 200
         else:
             resp = jsonify(errors)
             resp.status_code = 500
             return resp
 
 
-
 @app.route('/post/<post_id>', methods=['POST'])
 def upload_post_photo(post_id):
     if request.method == 'POST':
         if 'files[]' not in request.files:
-            resp = jsonify({'message': 'No file part in the request'})
-            resp.status_code = 400
-            return resp
+            return jsonify({'message': 'No file part in the request'}), 422
         files = request.files.getlist('files[]')
+
         errors = {}
         success = False
-        if not os.path.exists(POST_UPLOAD_FOLDER + '/' + post_id):
-            os.makedirs(POST_UPLOAD_FOLDER + '/' + post_id)
-
-        data = {"photos": []}
-
+        data_photos = {"photos": []}
         for file in files:
-            if file and allowed_file(file.filename):
-                path, dirs, files = next(os.walk(POST_UPLOAD_FOLDER + '/' + post_id))
-                file_count = str(len(files) + 1)
-                new_name = 'post(' + file_count + ').webp'
-                file.save(os.path.join(POST_UPLOAD_FOLDER + '/' + post_id, new_name))
-                road = 'http://' + HOST + ":" + str(PORT) + '/' + POST_UPLOAD_FOLDER + '/' + post_id + '/' + new_name
-                data["photos"].append(road)
+            if file:
+                data = file.read()
+                file_hash = hashlib.md5(data).hexdigest()
+                filename = file_hash[10:] + (uuid.uuid4().hex)[0:7] + datetime.now().strftime('%Y%m%d%H%M%S%f') + '.webp'
+                hash_road = str(file_hash[0:2]) + '/' + str(file_hash[2:4]) + '/' + str(file_hash[4:6]) + '/' + str(file_hash[6:8])
+                if not os.path.exists(POST_UPLOAD_FOLDER + '/' + hash_road):
+                    os.makedirs(POST_UPLOAD_FOLDER + '/' + hash_road)
+                f = open(POST_UPLOAD_FOLDER + '/' + hash_road + '/' + filename, "wb")
+                f.write(data)
+                f.close()
+                road = POST_UPLOAD_ALIAS + '/' + hash_road + '/' + filename
+                data_photos["photos"].append(road)
                 success = True
 
             else:
                 errors[file.filename] = 'File type is not allowed'
 
         if success and errors:
-            errors['message'] = 'File(s) successfully uploaded'
+            errors['message'] = 'something wrong'
             resp = jsonify(errors)
             resp.status_code = 500
-            valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
-
-            con = psycopg2.connect(
-                database=database,
-                user=user,
-                password=password,
-                host=host,
-                port=port
-            )
-            cur = con.cursor()
-            row = '"photo"'
-            cur.execute(
-                "UPDATE public.posts SET " + row + " = '" + valid_data + "' WHERE id = " + post_id
-            )
-            con.commit()
-            con.close()
-
             return resp
-
         if success:
-
-            resp = jsonify({'message': 'Files successfully uploaded'})
-            resp.status_code = 201
-            valid_data = '"' + (str(data).replace("'", '\\"')).replace(" ", "") + '"'
-
+            valid_data = '"' + (str(data_photos).replace("'", '\\"')).replace(" ", "") + '"'
             con = psycopg2.connect(
                 database=database,
                 user=user,
@@ -175,9 +134,7 @@ def upload_post_photo(post_id):
             )
             con.commit()
             con.close()
-
-            return resp
-
+            return jsonify({'message': 'success', 'images': data_photos}), 200
         else:
 
             resp = jsonify(errors)
@@ -186,4 +143,4 @@ def upload_post_photo(post_id):
 
 
 if __name__ == '__main__':
-    app.run(debug=True, host=HOST, port=PORT)
+    app.run(debug=True, host='0.0.0.0')
